8aee9953b2305e3eefad3f03f19df3fcab94468c
cutlerwater <cutlerwater2@live.com>
2021-11-13 10:38:33 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Canvas } from '@react-three/fiber';
+import { Anim } from './Anim';
+
+function AnimationCanvas() {
+  return (
+    <Canvas
+      camera={{position: [0, 2, 10], fov: 75}}
+    >
+      <ambientLight />
+      <Anim />
+    </Canvas>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="anim">
+        <AnimationCanvas />
+      </div>
     </div>
   );
 }
